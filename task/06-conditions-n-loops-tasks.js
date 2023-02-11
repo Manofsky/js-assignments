a5679c94d15a6b950da292f8f01d344aa1d50e3b
Manofsky <clegrof@gmail.com>
2023-02-11 13:56:15 +0300

    refactor: optimize toNaryString func (conditions-n-loops)

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -432,14 +432,7 @@ function timespanToHumanString(startDate, endDate) {
  *    365, 10 => '365'
  */
 function toNaryString(num, n) {
-  let currentNum = num;
-  let result = '';
-  while (currentNum >= n) {
-    let remainder = currentNum % n;
-    result = remainder + result;
-    currentNum = Math.floor(currentNum / n);
-  }
-  return currentNum + result;
+  return (num).toString(n);
 }
 
 
